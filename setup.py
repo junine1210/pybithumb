713b0b5e59ccc1739381be346ab6e521dfe8c032
junine1210 <junine1210@gmail.com>
2019-12-23 01:14:14 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name            = 'pybithumb',
+    version         = '1.0.2',
+    description     = 'python wrapper for Bithumb API',
+    url             = 'https://github.com/sharebook-kr/pybithumb',
+    author          = 'Lukas Yoo, Brayden Jo',
+    author_email    = 'jonghun.yoo@outlook.com, pystock@outlook.com',
+    install_requires=['requests', 'pandas', 'bs4', 'html5lib'],
+    license         = 'MIT',
+    packages        = ['pybithumb'],
+    zip_safe        = False
+)
